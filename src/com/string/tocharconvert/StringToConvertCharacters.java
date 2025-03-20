14418af99ed37d5cc054ffbf87a0ad61ca8850a2
Rajesh Patil <rajeshpatil2846@gmail.com>
2025-03-20 12:18:17 +0530

    Update StringToConvertCharacters.java

--- src/com/string/tocharconvert/StringToConvertCharacters.java
+++ src/com/string/tocharconvert/StringToConvertCharacters.java
@@ -3,10 +3,14 @@ package com.string.tocharconvert;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//Q.] How do you convert a List<String> into a List<Character> using Java Streams?
+
 public class StringToConvertCharacters {
 
 	public static void main(String[] args) {
 
+	// In Java 9+, we can use List.of("Rajesh", "Krishn")
+	// But in Java 8, List.of() is NOT available.
         List<String> words = List.of("Rajesh", "Krishn");
 
 		// [R, a, j, e, s, h, K, r, i, s, h, n, a]
